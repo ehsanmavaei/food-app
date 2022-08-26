48fd6026f50ff69c747e2cdd0b062d3028ece5fc
Ehsan <ehsanmavaei@gmail.com>
2022-08-26 11:25:03 +0430

    Hero-section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HeadlineCards from './components/HeadlineCards';
 
 
 
 function App() {
   return (
+    <div>
     <Navbar/>
+    <Hero/>
+    <HeadlineCards/>
+    </div>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import { FaUserFriends, FaWallet } from "react-icons/fa";
 import { MdFavorite, MdHelp } from "react-icons/md";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(true)
+    const [nav, setNav] = useState(false)
     return (
         <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
             {/*left side*/}

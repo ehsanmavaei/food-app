ecdb86ffa4e4c36ee306c81fbd6443344f58d7d2
Ehsan <ehsanmavaei@gmail.com>
2022-08-26 09:09:11 +0430

    navbar and side

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 
 
 function App() {
   return (
-    <h1 className="text-2xl font-bold underline">
-      Hello world!
-    </h1>
+    <Navbar/>
   );
 }
 
